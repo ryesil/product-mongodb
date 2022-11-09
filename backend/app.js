5c380c7d6ccbf7d27a39002e9c720fd1c56b6ade
ryesil <ramazanysl@gmail.com>
2022-11-08 20:41:44 -0600

    made some updates

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-
+console.log(bodyParser)
 const mongodb=require("mongodb").MongoClient;
 
 
@@ -14,18 +14,28 @@ app.use(bodyParser.json());
 
 app.use('/images', express.static(path.join('backend/images')));
 
+//Adding headers
 app.use((req, res, next) => {
   // Set CORS headers so that the React SPA is able to communicate with this server
+  // Website to allow to connect
   res.setHeader('Access-Control-Allow-Origin', '*');
+  // Request methods to allow
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET,POST,PUT,PATCH,DELETE,OPTIONS'
   );
+  // Request headers to allow
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  // Pass to next layer of middleware
   next();
 });
 
 app.use('/products', productRoutes);
 app.use('/', authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('root')
+})
+
 
 app.listen(3100);
